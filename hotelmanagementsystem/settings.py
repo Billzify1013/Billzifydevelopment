880791728d12831e0538fb6071aeb9738fcdf107
Ubuntu <ubuntu@ip-172-31-46-93.eu-north-1.compute.internal>
2024-07-12 05:06:00 +0000

    update

--- hotelmanagementsystem/settings.py
+++ hotelmanagementsystem/settings.py
@@ -85,10 +85,10 @@ WSGI_APPLICATION = 'hotelmanagementsystem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hotelmanagementsystem',
+        'NAME': 'hotelmanagement',
         'USER':'postgres',
-        'PASSWORD':'1013',
-        'HOST':'localhost'
+        'PASSWORD':'postgres123',
+        'HOST':'hotelmanagements.c5ekyoys6y25.eu-north-1.rds.amazonaws.com'
     }
 }
 
@@ -129,17 +129,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_URL='/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = '/path/to/your/static/root/'
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "app/static"),
-    os.path.join(BASE_DIR, "app2/static"),
+    os.path.join(BASE_DIR, 'app/static'),
+    os.path.join(BASE_DIR, 'app2/static'),
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
